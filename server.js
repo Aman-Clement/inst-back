a22b75693e91dbdb6807c23217d2f538c4223be7
Aman <amanclementaranha@gmail.com>
2023-02-25 15:31:53 +0530

    ada

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(cors());
 //   });
 // }
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin','http://127.0.0.1:5173');
+  res.setHeader('Access-Control-Allow-Origin','https://aman-clement.github.io/');
   next();
 });
 
