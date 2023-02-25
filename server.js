5cd1e5a7efd5085ad55f2c4cc44a795821f50b76
Aman <amanclementaranha@gmail.com>
2023-02-25 15:34:37 +0530

    nw

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(cors());
 //   });
 // }
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin','https://aman-clement.github.io/api/login');
+  res.setHeader('Access-Control-Allow-Origin','https://aman-clement.github.io/phishy/');
   next();
 });
 
